5c2ee62a8040f715e664a1386a07fbb53cb84174
Brendan Lu <brendannlu5@gmail.com>
2023-12-08 18:24:03 +1100

    slow progress on utils

--- tdim/__init__.py
+++ tdim/__init__.py
@@ -1,1 +1,7 @@
-from .devel import _singular_vals_mat_to_tensor, _singular_vals_tensor_to_mat
+from .devel import (
+    m_product,
+    _default_transform,
+    _singular_vals_mat_to_tensor,
+    _singular_vals_tensor_to_mat,
+    tSVDM,
+)
--- tdim/devel.py
+++ tdim/devel.py
@@ -5,70 +5,95 @@ In development...
 import numpy as np
 from numpy.typing import ArrayLike
 from scipy.fft import dct, idct
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+def m_product(A, B, M, Minv):
+    """Kilmer et al. (2021) tensor m-product for order-3 tensors. See [1]
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+    assert (
+        A.shape[1] == B.shape[0] and A.shape[2] == B.shape[2]
+    ), "Non conforming dimensions"
+
+    hatA, hatB = M(A), M(B)
+
+    # hatC = np.einsum('mpi,pli->mli', hatA, hatB)
+    # the following is a quicker version of the above using numpy broadcasting
+    hatC = np.matmul(hatA.transpose(2, 0, 1), hatB.transpose(2, 0, 1)).transpose(1, 2, 0)
+    
+    return Minv(hatC)
 
 
 def _default_transform(t):
     """Wrapper around scipy fft to generate functions to perform $\times_3 M$ operations
     on order-3 tensors, where M is defined by the (scaled) Discrete Cosine Transform.
 
-    See [1] and [2].
+    As introduced by Kilmer et al. (2021) and applied by Mor et al. (2022)
 
     Parameters
     ----------
-        t: int
+        t : int
             The length of the transform
 
     Returns
     -------
-        fun_m: Callable[[ArrayLike], ArrayLike]
+        fun_m : Callable[[ArrayLike], ArrayLike]
             A function which expects an order-3 tensor as input, and applies DCT-II to
             each of the tubal fibres. This preserves the dimensions of the tensor.
 
-        inv_m: Callable[[ArrayLike], ArrayLike]
+        inv_m : Callable[[ArrayLike], ArrayLike]
             A tensor transform (the inverse of `fun_m`)
     """
 
     def M(x):
         # we expect the third dimensions of the tensor to match the transform length
-        assert len(x.shape) == 3, "Expecting order 3 tensor input"
-        assert x.shape[-1] == t, f"Expecting input shape to be ?x?x{t}"
+        assert len(x.shape) == 3, "Expecting order-3 tensor input"
+        assert x.shape[-1] == t, f"Expecting input dimensions to be ?x?x{t}"
 
-        return dct(x, type=2, n=t, axis=-1, norm="ortho")
+        # allows parallel workers up to os.cpu_count. See scipy fft for more details
+        return dct(x, type=2, n=t, axis=-1, norm="ortho", workers=-1)
 
     def Minv(x):
         # we expect the third dimensions of the tensor to match the transform length
-        assert len(x.shape) == 3, "Expecting order 3 tensor input"
-        assert x.shape[-1] == t, f"Expecting input shape to be ?x?x{t}"
+        assert len(x.shape) == 3, "Expecting order-3 tensor input"
+        assert x.shape[-1] == t, f"Expecting input dimensions to be ?x?x{t}"
 
-        return idct(x, type=2, n=t, axis=-1, norm="ortho")
+        # allows parallel workers up to os.cpu_count. See scipy fft for more details
+        return idct(x, type=2, n=t, axis=-1, norm="ortho", workers=-1)
 
     return M, Minv
 
 
 def _singular_vals_mat_to_tensor(mat, n, p, t):
-    """Decommpress $k \times t$ matrix of singular values into $\hat{S}$ from literature.
+    """Decompress $k \times t$ matrix of singular values into $\hat{S}$ from literature.
 
     Parameters
     ----------
-        mat: ArrayLike
-            $k \times t$ matrix representation
+        mat : ArrayLike, shape: (k, tt)
+            $k \times t$ matrix representation, the function checks if `tt == t`
 
-        n: int
+        n : int
             first dimension of output tensor
 
-        p: int
+        p : int
             second dimension of output tensor
 
-        t: int
+        t : int
             third dimensions of output tensor
 
     Returns
     -------
-        hatS: ArrayLike
+        hatS : ArrayLike, shape: (n, p, t)
             $n \times p \times t$ tensor representation
     """
     k = min(n, p)
-    assert k == mat.shape[0] and t == mat.shape[1]
+    assert k == mat.shape[0] and t == mat.shape[1], "Ensure conforming dimensions"
 
     hatS = np.zeros((n, p, t))
     for i in range(t):
@@ -84,12 +109,12 @@ def _singular_vals_tensor_to_mat(tensor):
 
     Parameters
     ----------
-        tensor: ArrayLike
+        tensor : ArrayLike, shape: (n, p, t)
             $n \times p \times t$ tensor representation
 
     Returns
     -------
-        mat: ArrayLike
+        mat : ArrayLike, shape: (min(n, p), t)
             $k \times t$ matrix representation of singular values, where
             $k = \min{(n,p)}$
     """
@@ -102,18 +127,49 @@ def _singular_vals_tensor_to_mat(tensor):
     return mat
 
 
-def tSVDM(A, M, Minv, *, hats=False, full_matrices=False, compact_svals=False):
+def tSVDM(A, M, Minv, *, keep_hats=False, full_matrices=True, compact_svals=False):
     """Return the t-SVDM decomposition from Kilmer et al. (2021). Currently, this is
-    basically identical to the implementation in https://github.com/UriaMorP/mprod_package
+    a modified version of the implementation at https://github.com/UriaMorP/mprod_package
     but we plan to update this in future, potentially adopting the TensorFlow framework.
 
-    See [1]
+    NOTE: For now, unlike some other implementations (Numpy), we will return the tensor
+    $V$ NOT $V^T$. However, transposes are well-defined under this tensor-product
+    framework. See [1]
 
     Parameters
     ----------
+        A : ArrayLike, shape: (n, p, t)
+            $n \times p \times t$ data tensor
+
+        M : Callable[[ArrayLike], ArrayLike]
+            A function which, given some order-3 tensor, returns it under some $\times_3$
+            invertible transformation.
+
+        MInv : Callable[[ArrayLike], ArrayLike]
+            The inverse transformation of M
+
+        keep_hats : bool, default=False
+            Setting to `True` will return the tSVDM factors in the tensor domain transform
+            space, under the specified $M$
+
+        full_matrices : bool, default=True
+            In practice, one only needs the first $k$ columns of $U_{:,:,i}$, $V_{:,:,i}$.
+            Setting this to False will return tensors truncated, by removing columns after
+            the k-th one in U or V.
+            See: https://numpy.org/doc/stable/reference/generated/numpy.linalg.svd.html
+
+        compact_svals : bool, default=False
+            Setting to `True` will return a compressed version of $S$, whereby the
+            singular values of each f-diagonal frontal slice becomes the column of a
+            matrix, with t columns total
 
     Returns
     -------
+        U_tens : ArrayLike, shape: (n, n, t) if full_matrices else (n, k, t)
+
+        S_tens : ArrayLike, shape: (n, p, t) if not compact_svals else (k, t)
+
+        V_tens : ArrayLike, shape: (p, p, t) if full_matrices else (p, k, t)
     """
 
     # transform the tensor to new space via the mode-3 product
@@ -131,9 +187,9 @@ def tSVDM(A, M, Minv, *, hats=False, full_matrices=False, compact_svals=False):
     hatU = U_stack.transpose(1, 2, 0)
     hatV = V_stack.transpose(2, 1, 0)
 
-    if hats:
-        # by default, convert into n,p,t tensor 
-        # (optionally) compressed matrix of singular values of size k,t
+    if keep_hats:
+        # by default return S as n,p,t f-diagonal tensor, matching literature
+        # (optionally) convert into compressed matrix of singular values of size k,t
         hatS = (
             _singular_vals_mat_to_tensor(S_mat.transpose(), *A.shape)
             if not compact_svals
@@ -141,7 +197,28 @@ def tSVDM(A, M, Minv, *, hats=False, full_matrices=False, compact_svals=False):
         )
         return hatU, hatS, hatV
     else:
+        # regardless of compact_svals setting, S needs to be converted into tensor form
+        # for Minv to be applied
+        # NOTE: there may be a more efficient way to do this
         S = Minv(_singular_vals_mat_to_tensor(S_mat.transpose(), *A.shape))
         S = _singular_vals_tensor_to_mat(S) if compact_svals else S
         U, V = Minv(hatU), Minv(hatV)
         return U, S, V
+
+
+class TCAM(BaseEstimator, TransformerMixin):
+    """Mor et al. (2020) t-SVDM tensor analogue of PCA using explicit rank truncation
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
+
+    def __init__(self, M, Minv):
+        self.M = M
+        self.Minv = Minv
+
+    def _mprod(self, A, B):
+        pass
