a8b2e344557f91ca2838797e2c2ccbe468b2e5b6
BLuLaptop <brendannlu5@gmail.com>
2023-12-15 12:24:31 +1100

    minor: fix up __init__

--- tred/__init__.py
+++ tred/__init__.py
@@ -1,15 +1,15 @@
 """
-In development...
-
-The :mod:`tdim` includes relatively new tensor decomposition methods. Most of the 
-algorithms in this module can be regarded as dimensionality reduction techniques, for 
-order-3 data. 
+The :mod:`tred` module implements tensor decomposition methods such as tensor PCA and 
+tensor SVD. Most of the functionality in this module can be regarded as dimensionality
+reduction strategies for order-3 datasets of size n, p, t, where p >> n > t. 
 """
 
 from ._tensor_pca import tsvdm, TPCA
 from ._utils import display_tensor_facewise
 from ._m_transforms import generate_DCTii_M_transform_pair
 
-# for testing ----------------------------------------------------------------------------
+# 'private' ------------------------------------------------------------------------------
+# We let users access these utilities and operations if they so wish. In the future, we 
+# may mark many of these as public
 from ._utils import _singular_vals_mat_to_tensor, _singular_vals_tensor_to_mat
 from ._ops import _m_product, _mode0_unfold
