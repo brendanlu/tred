d55e1396993de37783b8270a7debbaed0f68626f
Brendan Lu <brendannlu5@gmail.com>
2024-01-15 01:56:58 +1100

    testing: add coverage for scipyfft-based transforms

--- tests/test_m_transforms.py
+++ tests/test_m_transforms.py
@@ -3,16 +3,25 @@ import pytest
 from scipy.stats import special_ortho_group
 from numpy.testing import assert_allclose
 
-from tred import generate_transform_pair_from_matrix
+from tred import (
+    generate_transform_pair_from_matrix,
+    generate_dctii_m_transform_pair,
+    generate_dstii_m_transform_pair,
+)
 
 GLOBAL_SEED = 1
 
 TENSOR_SHAPES = [(4, 3, 2), (5, 7, 6), (2, 2, 6)]
 MATRIX_SHAPES = [(4, 6), (7, 3)]
 
+TRANSFORM_FAMILY_GENERATORS = [
+    generate_dctii_m_transform_pair,
+    generate_dstii_m_transform_pair,
+]
+
 
 @pytest.mark.parametrize("tensor_shape", TENSOR_SHAPES)
-def test_generate_transform_pair_from_matrix_for_tensor(tensor_shape):
+def test_generate_transform_pair_from_matrix_for_tensor_target(tensor_shape):
     """Compare implementation with (slower) mathematically clear version"""
     # scaling constant (arbitrary)
     C = 5
@@ -30,12 +39,16 @@ def test_generate_transform_pair_from_matrix_for_tensor(tensor_shape):
         for j in range(p):
             hatX_expected[i, j, :] = M_mat @ X[i, j, :]
 
-    M, _ = generate_transform_pair_from_matrix(M_mat)
+    # compare with our optimized implementation
+    M, Minv = generate_transform_pair_from_matrix(M_mat)
     assert_allclose(hatX_expected, M(X))
+
+    # test inverse transform working as expected
+    assert_allclose(X, Minv(M(X)))
 
 
 @pytest.mark.parametrize("matrix_shape", MATRIX_SHAPES)
-def test_generate_transform_pair_from_matrix_for_matrix(matrix_shape):
+def test_generate_transform_pair_from_matrix_for_matrix_target(matrix_shape):
     """Compare implementation with (slower) mathematically clear version"""
     # scaling constant (arbitrary)
     C = 5
@@ -52,5 +65,29 @@ def test_generate_transform_pair_from_matrix_for_matrix(matrix_shape):
     for i in range(k):
         hatX_expected[i, :] = M_mat @ X[i, :]
 
-    M, _ = generate_transform_pair_from_matrix(M_mat)
+    # compare with our optimized implementation
+    M, Minv = generate_transform_pair_from_matrix(M_mat)
     assert_allclose(hatX_expected, M(X))
+
+    # test inverse transform working as expected
+    assert_allclose(X, Minv(M(X)))
+
+
+@pytest.mark.parametrize("shape", TENSOR_SHAPES + MATRIX_SHAPES)
+@pytest.mark.parametrize("transform_generator", TRANSFORM_FAMILY_GENERATORS)
+def test_scipy_fft_wrapper_transforms(shape, transform_generator):
+    # scaling constant (arbitrary)
+    C = 5
+
+    rng = np.random.default_rng(seed=GLOBAL_SEED)
+    t = shape[-1]
+
+    # arrays of various sizes with uniformly distributed elements within [-0.5*C, 0.5*C)
+    X = rng.random(size=shape) * C - 0.5 * C
+    M, Minv = transform_generator(t)
+
+    # ensure transform does not change shape
+    assert M(X).shape == X.shape
+
+    # test inverse transform working as expected
+    assert_allclose(X, Minv(M(X)))
